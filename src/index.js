f237cb331bbd51949d4e250115b1aaf5ae68eb68
Alexander <guts-817@rambler.ru>
2018-02-26 11:10:17 +0300

    my decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    // your solution
+
+    for (var i=0; i<9; i++) {
+        for (var j=0; j<9; j++) {
+            if (matrix[i][j]==0)
+                matrix[i][j]=[];
+        }
+    }
+
+    return matrix;
 }
